79a80a12a84b970d8ff218db986d9627f33313e1
achrafedd <achrafeddiba@gmail.com>
2023-09-11 12:25:37 +0100

    add main file

---
+++ main.c
@@ -0,0 +1,15 @@
+#include "shell.h"
+
+/**
+ * main - main shell function
+ *
+ * @ac: arguments count
+ * @av: arguments
+ *
+ * Return: 0 if programme run correctly, non zero in error
+ */
+
+int main(int ac, char **av)
+{
+	return (0);
+}
---
+++ shell.h
@@ -0,0 +1,15 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/wait.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+
+
+
+
+#endif
